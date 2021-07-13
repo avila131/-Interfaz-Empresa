6cbf4fa5b303ad76516b1538d4adb678aa6f704d
edgonzalezdi <82072189+edgonzalezdi@users.noreply.github.com>
2021-07-13 10:41:51 -0500

    Update Form7.Designer.cs

--- WindowsFormsApplication2/Form7.Designer.cs
+++ WindowsFormsApplication2/Form7.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication2
+namespace WindowsFormsApplication2
 {
     partial class Form7
     {
@@ -30,10 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -61,27 +61,16 @@
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             // 
-            // button1
+            // button4
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(14, 33);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(279, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Archivo Resultado - Informe Final";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(15, 115);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(279, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Perforaciones - Muestras";
-            this.button2.UseVisualStyleBackColor = false;
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(14, 73);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(279, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ensayos a Muestras";
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
@@ -93,17 +82,31 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Tipos de ensayos";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // button2
             // 
-            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
-            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(14, 73);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(279, 32);
-            this.button4.TabIndex = 3;
-            this.button4.Text = "Ensayos a Muestras";
-            this.button4.UseVisualStyleBackColor = false;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(15, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(279, 32);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Perforaciones - Muestras";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(14, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(279, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Archivo Resultado - Informe Final";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button5
             // 
@@ -115,6 +118,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "Cerrar Sesión";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // Form7
             // 
