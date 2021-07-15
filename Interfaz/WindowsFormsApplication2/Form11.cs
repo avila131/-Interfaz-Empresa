804b3ea51fd265b1e683b0fc6bc7cbdba8fec7ab
José Ávila <60366097+avila131@users.noreply.github.com>
2021-07-14 21:56:21 -0500

    Simplificado Form11

---
+++ Interfaz/WindowsFormsApplication2/Form11.cs
@@ -0,0 +1,180 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form11 : Form
+    {
+        public MySqlDataReader reader;
+        public string Perforacion_ID, Muestra_ID, tipoEnsayo_ID, ensayoMuestra_ID, Proyecto_ID;
+
+
+        private void MostrarDatosActualizadosEnPantalla()
+        {
+            txtNombreProyectoSeleccionado.Text = obtenerNombreProyectoActual();
+            txtNombrePerforacionSeleccionada.Text = obtenerNombrePerforacionActual();
+            txtNumeroMuestraSeleccionada.Text = obtenerNumeroMuestraActual();
+            txtTipoEnsayoSeleccionado.Text = obtenerNombreTipoEnsayoActual();
+            txtEstadoEnsayoMuestraSeleccionado.Text = obtenerEstadoEnsayoMuestraActual();
+        }
+
+
+        private string obtenerNombreProyectoActual()
+        {
+            string query = "SELECT NombreProyecto FROM vw_idProyecto_nombreProyecto_estadoProyecto WHERE ID = " + Proyecto_ID + ";";
+            string nombreObtenido = ExecuteScalarReader(query);
+            return nombreObtenido == "NULL" ? "Sin registros para mostrar" : nombreObtenido; 
+        }
+
+
+        private void btnSiguientePerforacion_Click(object sender, EventArgs e)
+        {
+            string query = "select per_idPerforacion from perforacion where per_idPerforacion = (select min(per_idPerforacion) from perforacion where per_idPerforacion > " + Perforacion_ID + " AND pro_idProyecto =" + Proyecto_ID + ");";  // Obtener id siguiente perforación
+            if (ExecuteScalarReader(query) == "NULL")  // Se sale de los límites
+                return;
+            Perforacion_ID = ExecuteScalarReader(query);
+
+            actualizar_ID_Muestra();
+            actualizar_ID_TipoEnsayo();
+
+            query = "SELECT per_nombrePerforacion FROM perforacion WHERE per_idPerforacion = " + Perforacion_ID + ";";
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private string ExecuteScalarReader(string given_query)
+        {
+            MySqlCommand command = Program.getNewMySqlCommand(given_query);
+            try
+            { return command.ExecuteScalar().ToString(); }
+            catch
+            { return "NULL"; }
+        }
+
+
+        private string obtenerNombrePerforacionActual()
+        {
+            string query = "SELECT per_nombrePerforacion FROM perforacion WHERE per_idPerforacion = "+ Perforacion_ID + ";";
+            string nombreObtenido = ExecuteScalarReader(query);
+            return nombreObtenido == "NULL" ? "Sin registros para mostrar" : nombreObtenido;
+        }
+
+
+        private string obtenerNombreTipoEnsayoActual()
+        {
+            string query = "SELECT tip_nombreTipoEnsayo FROM tipoensayo WHERE tip_idTipoEnsayo = " + tipoEnsayo_ID + ";";
+            string nombreObtenido = ExecuteScalarReader(query);
+            return nombreObtenido == "NULL" ? "Sin registros para mostrar" : nombreObtenido;
+        }
+
+
+        private void btnAnteriorPerforacion_Click(object sender, EventArgs e)
+        {
+            string query = "select per_idPerforacion from perforacion where per_idPerforacion = (select max(per_idPerforacion) from perforacion where per_idPerforacion < " + Perforacion_ID + " AND pro_idProyecto =" + Proyecto_ID + ");";  // Obtener id anterior perforación
+            if (ExecuteScalarReader(query) == "NULL")
+                return;
+            Perforacion_ID = ExecuteScalarReader(query);
+            actualizar_ID_Muestra();
+            actualizar_ID_TipoEnsayo();
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private void btnAnteriorMuestra_Click(object sender, EventArgs e)
+        {
+            string query = "select mue_idMuestra from muestra where mue_idMuestra = (select max(mue_idMuestra) from muestra where mue_idMuestra < " + Muestra_ID + " AND per_idPerforacion = " + Proyecto_ID + ");";
+            if (ExecuteScalarReader(query) == "NULL")  // Se sale de los límites
+                return;
+            Muestra_ID = ExecuteScalarReader(query);
+            actualizar_ID_TipoEnsayo();
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private void btnSiguienteMuestra_Click(object sender, EventArgs e)
+        {
+            string query = "select mue_idMuestra from muestra where mue_idMuestra = (select min(mue_idMuestra) from muestra where mue_idMuestra > " + Muestra_ID + ");";
+            if (ExecuteScalarReader(query) == "NULL")  // Se sale de los límites
+                return;
+            Muestra_ID = ExecuteScalarReader(query);
+            actualizar_ID_TipoEnsayo();
+
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private void actualizar_ID_Muestra()
+        {
+            string query = "SELECT mue_idMuestra FROM muestra WHERE per_idPerforacion = " + Perforacion_ID + " ORDER BY mue_idMuestra LIMIT 1;";
+            Muestra_ID = ExecuteScalarReader(query);
+        }
+
+
+        private void actualizar_ID_EnsayoMuestra()
+        {
+            string query = "SELECT ens_idEnsayoMuestra FROM ensayomuestra WHERE mue_idMuestra = " + Muestra_ID + " ORDER BY ens_idEnsayoMuestra LIMIT 1;";
+            ensayoMuestra_ID = ExecuteScalarReader(query);
+        }
+
+
+        private void btnSiguienteEnsayo_Click(object sender, EventArgs e)
+        {
+            string query = "select tip_idTipoEnsayo from tipoensayo NATURAL JOIN ensayomuestra where tip_idTipoEnsayo = (select min(tip_idTipoEnsayo) from tipoensayo where tip_idTipoEnsayo > " + tipoEnsayo_ID + ") AND mue_idMuestra = " + Muestra_ID + ";";
+            if (ExecuteScalarReader(query) == "NULL")  // Se sale de los límites
+                return;
+            tipoEnsayo_ID = ExecuteScalarReader(query);
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private void actualizar_ID_TipoEnsayo()
+        {
+            string query = "SELECT tip_idTipoEnsayo FROM ensayomuestra NATURAL JOIN tipoensayo WHERE mue_idMuestra =" + Muestra_ID + " ORDER BY tip_idTipoEnsayo LIMIT 1;";
+            tipoEnsayo_ID = ExecuteScalarReader(query);
+        }
+
+
+        private void btnAnteriorEnsayo_Click(object sender, EventArgs e)
+        {
+            string query = "select tip_idTipoEnsayo from tipoensayo NATURAL JOIN ensayomuestra where tip_idTipoEnsayo = (select max(tip_idTipoEnsayo) from tipoensayo where tip_idTipoEnsayo < " + tipoEnsayo_ID + ") AND mue_idMuestra = " + Muestra_ID + ";";
+            if (ExecuteScalarReader(query) == "NULL")  // Se sale de los límites
+                return;
+            tipoEnsayo_ID = ExecuteScalarReader(query);
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        private string obtenerNumeroMuestraActual()
+        {   
+            string query = "SELECT mue_numeroMuestra FROM muestra WHERE mue_idMuestra = " + Muestra_ID + ";";
+            string nombreObtenido = ExecuteScalarReader(query);
+            return nombreObtenido == "NULL" ? "Sin registros para mostrar" : nombreObtenido;
+        }
+
+
+        private string obtenerEstadoEnsayoMuestraActual()
+        {
+            string query = "select ens_estado FROM ensayomuestra WHERE ens_idEnsayoMuestra = " + ensayoMuestra_ID +";";
+            string nombreObtenido = ExecuteScalarReader(query);
+            return nombreObtenido == "NULL" ? "Sin registros para mostrar" : nombreObtenido;
+        }
+
+
+        private void Form11_Load(object sender, EventArgs e)
+        {
+            MostrarDatosActualizadosEnPantalla();
+        }
+
+
+        public Form11(string given_muestra_ID, string given_perforacion_ID, string given_proyecto_ID)
+        {
+            Muestra_ID = given_muestra_ID;
+            Perforacion_ID = given_perforacion_ID;
+            Proyecto_ID = given_proyecto_ID;
+            actualizar_ID_TipoEnsayo();
+            actualizar_ID_EnsayoMuestra();
+            InitializeComponent();
+        }
+    }
+}
---
+++ Interfaz/WindowsFormsApplication2/Form9.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form9 : Form
+    {
+        public bool UserSuccessfullyAuthenticated { get; private set; }
+        public string userRole;
+
+        public MySqlDataReader reader;
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        public bool canOpenConnection()
+        {
+            try
+            {
+                string connectionString = "datasource=localhost;port=3306;username='" + textBox1.Text
+                    + "';password='" + textBox2.Text + "';database=mydb;";
+                Program.databaseConnection = new MySqlConnection(connectionString);
+                Program.databaseConnection.Open();
+                return true;
+            }
+            catch { return false; }
+        }
+        private void Form9_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string connectionString = "datasource=localhost;port=3306;username='" + textBox1.Text
+                + "';password='" + textBox2.Text + "';database=mydb;convert zero datetime=True";
+            if (canOpenConnection())
+            {
+                UserSuccessfullyAuthenticated = true;
+                MessageBox.Show("Conexión exitosa");
+                try
+                {
+                    string query = "SELECT CURRENT_ROLE()";
+                    MySqlCommand commandDatabase = Program.getNewMySqlCommand(query);
+                    userRole = (String)commandDatabase.ExecuteScalar();
+                }catch
+                {
+                    MessageBox.Show("No se pudo hacer la lectura del rol del usuario");
+                }
+            }   
+            else
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            this.Close();
+        }
+    }
+}
